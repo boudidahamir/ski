6e72a43e2878ecc1483a1d9bd386aae288536e57
arwa255 <80281987+arwa255@users.noreply.github.com>
2024-11-03 19:43:01 +0100

    Update RegistrationNotFoundException.java

--- src/main/java/tn/esprit/spring/exceptions/RegistrationNotFoundException.java
+++ src/main/java/tn/esprit/spring/exceptions/RegistrationNotFoundException.java
@@ -1,1 +1,9 @@
+package tn.esprit.spring.exceptions;
+
+public class RegistrationNotFoundException extends RuntimeException {
+    public RegistrationNotFoundException(String message) {
+        super(message);
+    }
+
+}
 
